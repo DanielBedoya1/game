3c3b5b711ca3104ea925a79e5ce136505731378c
DanielBedoya1 <113715199+DanielBedoya1@users.noreply.github.com>
2023-02-08 13:27:19 -0500

    Add files via upload

---
+++ pygame/click_game.py
@@ -0,0 +1,18 @@
+blob = Actor('character')
+blob.pos = 100,100
+
+WIDTH = 500
+HEIGHT = 300
+BACKGROUND_COLOR = (255,0,0)
+
+def draw():
+    screen.fill(BACKGROUND_COLOR)
+    blob.draw()
+def update():
+    blob.left = blob.left + 2
+    if blob.left > WIDTH:
+        blob.right = 0
+
+
+
+
