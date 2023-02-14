454e50f58aef9fecfb1e45fb9fdd6aa2a55b4f3b
DanielBedoya1 <113715199+DanielBedoya1@users.noreply.github.com>
2023-02-14 13:36:33 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -1,4 +1,5 @@
 blob = Actor('character')
+bloboc = Actor('character_clicked')
 blob.pos = 100,100
 
 WIDTH = 500
@@ -12,6 +13,20 @@ def update():
     blob.left = blob.left + 2
     if blob.left > WIDTH:
         blob.right = 0
+
+def on_mouse_down(pos):
+    if blob.collidepoint(pos):
+        set_blob_hurt()
+
+def set_blob_hurt():
+    blob.image = 'character_clicked'
+    sounds.ouch.play()
+    clock.schedule_unique(set_blob_normal, 1.0)
+
+
+def set_blob_normal():
+    blob.image = 'character'
+
 
 
 
